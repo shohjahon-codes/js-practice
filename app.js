4bf48d8dc069ff794260662bb46ef8dfcbd421aa
Shohjahon <154497887+shohjahon-codes@users.noreply.github.com>
2024-01-15 07:20:28 +0500

    masalalarim

---
+++ app.js
@@ -0,0 +1,107 @@
+// let  son = +prompt ("Raqam Kiriting")
+ 
+// if( son > 0){
+//     son = son + 1
+    
+// }
+// alert( " javob" + son)
+// let  son = +prompt ("Raqam Kiriting")
+ 
+// if( son > 0 ){
+//     son = son + 1
+    
+//     alert( " javob" + son)
+// }
+
+// else {
+//     son = son - 2
+//     alert( " else" + son)
+// }
+// let num = prompt ("raqam kiriting")
+// if(num % 2 == 0){
+//     console.log("butun")
+// }else{
+//     console.log("haqiqiy")
+// }
+// function nechtaButunSon(son1, son2, son3) {
+//     var butunSonlar = 0;
+
+//     if (Number.isInteger(son1) && son1 >= 0) {
+//         butunSonlar++;
+//     }
+
+//     if (Number.isInteger(son2) && son2 >= 0) {
+//         butunSonlar++;
+//     }
+
+//     if (Number.isInteger(son3) && son3 >= 0) {
+//         butunSonlar++;
+//     }
+
+//     return butunSonlar;
+// }
+
+// var son1 = prompt("Iltimos, birinchi sonni kiriting:");
+// var son2 = prompt("Iltimos, ikkinchi sonni kiriting:");
+// var son3 = prompt("Iltimos, uchunchi sonni kiriting:");
+
+// son1 = parseFloat(son1);
+// son2 = parseFloat(son2);
+// son3 = parseFloat(son3);
+
+// if (!isNaN(son1) && !isNaN(son2) && !isNaN(son3)) {
+//     var nechtaButunSonlar = nechtaButunSon(son1, son2, son3);
+//     console.log("Uchta kiritilgan sonning " + nechtaButunSonlar + " ta butun sonligi bor.");
+// } else {
+//     console.log("Noto'g'ri kiritim. Iltimos, sonlarni kiriting.");
+// }
+
+
+// 4-5-7- masalalar
+
+
+
+
+// var son1 = prompt("Iltimos, birinchi sonni kiriting:");
+// var son2 = prompt("Iltimos, ikkinchi sonni kiriting:");
+
+// son1 = parseFloat(son1);
+// son2 = parseFloat(son2);
+
+// if (!isNaN(son1) && !isNaN(son2)) {
+//     if (son1 > son2) {
+//         console.log(son1 + " soni " + son2 + " dan katta.");
+//     } else if (son1 < son2) {
+//         console.log(son2 + " soni " + son1 + " dan katta.");
+//     } else {
+//         console.log("Sonlar o'zaro teng.");
+//     }
+// } else {
+//     console.log("Noto'g'ri kiritim. Iltimos, sonlarni kiriting.");
+// }
+
+
+// 8- masala 
+// let A = prompt("Iltimos, A ni kiriting:");
+// let B = prompt("Iltimos, B ni kiriting:");
+
+// A = parseInt(A);
+// B = parseInt(B);
+
+// if (!isNaN(A) && !isNaN(B)) {
+//     if (A !== B) {
+//         A = A + B;
+//         B = A;
+//     } else {
+//         A = 0;
+//         B = 0;
+//     }
+
+//     console.log("Yangi A qiymati: " + A);
+//     console.log("Yangi B qiymati: " + B);
+// } else {
+//     console.log("Noto'g'ri kiritim. Iltimos, butun sonlarni kiriting.");
+// }
+
+// 9- masala
+
---
+++ script.js
@@ -0,0 +1,9 @@
+let son = -1
+
+if(son > 0){
+    son = son + 1
+}
+
+console.log(son)
+
+
